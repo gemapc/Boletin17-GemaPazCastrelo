d4898e30c0d49867f87761a7f5be633861f7390e
gemapc <paz.castrelo.gema@gmail.com>
2017-05-11 10:36:01 +0200

    V7

--- Boletin17-JUnit/src/DNI.java
+++ Boletin17-JUnit/src/DNI.java
@@ -1,8 +1,17 @@
 import java.util.ArrayList;
 
+/**
+ * @author Gema Paz Castrelo
+ */
+
 public class DNI {
 	private char letras[]={'T','R','W','A','G','M','Y','F','P','D','X','B','N','J','Z','S','Q','V','H','L','C','K','E'};
 	
+	/**
+	 * Método que valora se un DNI é válido ou non, se o número se corresponde coa letra.
+	 * @param numeroLetra String co valor do NIF, é dicir, coa letra.
+	 * @return true se o valor do DNI se corresponde coa letra, false en caso contrario.
+	 */
 	public boolean eValido(String numeroLetra){
 		if(numeroLetra.length()<2 || numeroLetra.length()>9) return false;
 		
@@ -20,6 +29,11 @@ public class DNI {
 		return false;
 	}
 	
+	/**
+	 * Método que calcula a letra válida para un número DNI.
+	 * @param numero String co número DNI sobre o que se quere calcular a letra correspondente.
+	 * @return O valor int correspondente á posición da letra.
+	 */
 	public int calculaLetra(String numero){
 		if(numero.length()<1 || numero.length()>8) return -1;
 		
@@ -32,6 +46,12 @@ public class DNI {
 		return -1;
 	}
 	
+	/**
+	 * Método que valora se un DNI é válido ou non, se o número se corresponde coa letra.
+	 * @param numeros ArrayList de enteiros co número do DNI.
+	 * @param letra Letra a probar a súa validez co número.
+	 * @return true se a letra se corresponde co número, false en caso contrario.
+	 */
 	public boolean eValido(ArrayList<Integer> numeros, char letra){
 		if(numeros.size()<1 || numeros.size()>8) return false;
 		
@@ -47,6 +67,12 @@ public class DNI {
 		return false;		
 	}
 	
+	/**
+	 * Método que calcula a letra válida para un número DNI.
+	 * @param numeros ArrayList de enteiros co número DNI sobre o 
+	 * que se quere calcular a letra correspondente.
+	 * @return O valor int correspondente á posición da letra.
+	 */
 	public int calculaLetra(ArrayList<Integer> numeros){
 		if(numeros.size()<1 || numeros.size()>8) return -1;
 		
--- Boletin17-JUnit/src/Xeometria.java
+++ Boletin17-JUnit/src/Xeometria.java
@@ -1,21 +1,45 @@
+/**
+ * @author Gema Paz Castrelo
+ */
 
 public class Xeometria {
-	
+	/**
+	 * Calcula o perímero de un cadrado.
+	 * @param lado Medida do lado do cadrado.
+	 * @return O valor do perímetro.
+	 */
 	public int perimetroCadrado(int lado){
 		if(lado<0) return -1;
 		return lado*4;
 	}
 	
+	/**
+	 * Calcula a área de un cadrado.
+	 * @param lado Medida do lado do cadrado.
+	 * @return O valor da área.
+	 */
 	public int areaCadrado(int lado){
 		if(lado<0) return -1;
 		return lado*2;
 	}
 	
+	/**
+	 * Calcula a hipotenusa mediante o teorema de pitágoras.
+	 * @param catetoA Valor de un dos catetos.
+	 * @param catetoB Valor de outro cateto.
+	 * @return O valor da hipotenusa.
+	 */
 	public double teoremaPitagoras(int catetoA, int catetoB){
 		if(catetoA<0 || catetoB<0) return -1;
 		return Math.sqrt(catetoA*catetoA+catetoB*catetoB);
 	}
 	
+	/**
+	 * Calcula o valor de un dos catetos mediante o teorema de pitágoras.
+	 * @param hipotenusa Valor da hipotenusa.
+	 * @param catetoA Valor do outro cateto.
+	 * @return O valor do cateto restante.
+	 */
 	public double teoremaPitagoras(double hipotenusa, double catetoA){
 		if(catetoA<0 || hipotenusa<0) return -1;
 		else if(hipotenusa<catetoA) return -1;
